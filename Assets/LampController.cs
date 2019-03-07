f52712330926fe48eb376cb94c870648f00ea35d
Tim R <tim.rickers@sap.com>
2019-03-07 17:38:44 +0100

    add button

---
+++ Assets/LampController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampController : MonoBehaviour
+{
+    public buttonController input;
+    
+    void Update()
+    {
+        if (input.isActive)
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        else
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+    }
+}
---
+++ Assets/buttonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class buttonController : MonoBehaviour
+{
+    public bool isActive = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isActive = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isActive = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isActive = true;
+    }
+}
