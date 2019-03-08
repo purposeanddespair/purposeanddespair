212e2c626cd3664047c2cdc82147c48c36080c48
gobuhl <gobuhl07@web.de>
2019-03-08 11:33:19 +0100

    first level

---
+++ Assets/doorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour
+{
+    public buttonController input;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform door = gameObject.transform.GetChild(0);
+        Vector3 closePosition = door.transform.position;
+        Vector3 openPosition = closePosition;
+        openPosition.x += 20;
+        if (input.isActive)
+            door.transform.position = openPosition;
+        else
+            door.transform.position = closePosition;
+    }
+}
