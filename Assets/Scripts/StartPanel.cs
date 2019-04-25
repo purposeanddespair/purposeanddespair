fc78b04d2a56eb9f26d02e0094fddd76d69207dc
Helco <hermann.noll@hotmail.com>
2019-04-25 16:26:27 +0200

    Add start and end panel

---
+++ Assets/Scripts/StartPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPanel : MonoBehaviour
+{
+    public PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.lockMovement = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.lockMovement = false;
+            Destroy(gameObject);
+        }
+    }
+}
